0c28dce1fbce4aa52ea7cec382f2933dff70a5ca
Huynh Duc Dung <dunghd.it@gmail.com>
2022-07-15 18:32:08 +0800

    refactor(deps): upgrade web3js v2 alpha
    BREAKING CHANGE: This is the beta release for testing with web3 v2

--- example/index.ts
+++ example/index.ts
@@ -1,10 +1,17 @@
-import {config} from 'dotenv';
-
-import {getReasonByWeb3Js} from '../src';
-
-config();
+import {getReasonByEthersJs, getReasonByWeb3Js} from '../src';
 
 getReasonByWeb3Js(
+	'0x35403cfc33cababac41ad998dff9038c57945ac7cbe9f22e5bdfbf89a8756bd7',
+	'https://data-seed-prebsc-1-s1.binance.org:8545',
+)
+	.then(reason => {
+		console.log(reason);
+	})
+	.catch(error => {
+		console.log(error);
+	});
+
+getReasonByEthersJs(
 	'0x35403cfc33cababac41ad998dff9038c57945ac7cbe9f22e5bdfbf89a8756bd7',
 	'https://data-seed-prebsc-1-s1.binance.org:8545',
 )
